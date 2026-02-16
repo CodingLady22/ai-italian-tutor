c9f70fe8f7345a2ed3486980404d90a1ceda6c02
Maye Jesuorobo <nayawes@yahoo.com>
2026-02-16 10:58:39 +0100

    update client side url for CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://ai-italian-tutor.vercel.app/', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
   });
