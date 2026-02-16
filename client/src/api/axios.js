4118275e322c4a11246190547db42700c764525d
Maye Jesuorobo <nayawes@yahoo.com>
2026-02-16 10:19:25 +0100

    update baseURL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'ai-italian-tutor-production.up.railway.app',
     headers: {
         "Content-Type": "application/json"
     }
